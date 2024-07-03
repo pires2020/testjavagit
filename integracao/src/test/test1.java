e11ecb81f078feb95fc73916a07ddd029818400c
Lucas Antunes Pires <64935655+pires2020@users.noreply.github.com>
2024-07-03 15:09:02 -0300

    Update test1.java

--- integracao/src/test/test1.java
+++ integracao/src/test/test1.java
@@ -4,7 +4,9 @@ public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello World");
+                System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("socorro");
 	}
 
 }
